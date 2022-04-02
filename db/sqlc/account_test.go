c82fa75b19929914c3b5f6f03708a6f9f06b5ba5
Anshu <alchsyss@pop-os.localdomain>
2022-04-02 10:32:21 +0530

    account_test.go added

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	_ "context"
+	"testing"
+
+	_ "github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "tom",
+		Balance:  100,
+		Currency: "USD",
+	}
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/anshugupta673/banking-application
 
 go 1.12
+
+require (
+	github.com/lib/pq v1.10.4
+	github.com/stretchr/testify v1.7.1
+)
