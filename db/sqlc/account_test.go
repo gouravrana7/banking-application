a0021cd3760735032d191762f15d435185741c3c
Anshu <alchsyss@pop-os.localdomain>
2022-04-02 11:00:07 +0530

    random accounts table data for db unit test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,17 +1,34 @@
 package db
 
 import (
+	"context"
 	_ "context"
 	"testing"
 
+	"github.com/anshugupta673/banking-application/util"
+	"github.com/stretchr/testify/require"
 	_ "github.com/stretchr/testify/require"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "tom",
-		Balance:  100,
-		Currency: "USD",
+		// Owner:    "tom",
+		// Balance:  100,
+		// Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
 }
